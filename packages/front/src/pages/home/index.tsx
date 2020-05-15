a8812a7642365eaf4cd318fe75fd7a29860b9c37
Adrien Mazet <adrien.mazet@imt-atlantique.net>
2020-05-15 17:22:11 +0200

    Navigation and layout setup

---
+++ packages/front/src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import CustomComponent from "components/custom";
+import React from "react";
+
+const Home = () => (
+  <>
+    <CustomComponent title="Home" />
+  </>
+);
+
+export default Home;
---
+++ packages/front/src/pages/record/index.tsx
@@ -0,0 +1,10 @@
+import CustomComponent from "components/custom";
+import React from "react";
+
+const Record = () => (
+  <>
+    <CustomComponent title="Record" />
+  </>
+);
+
+export default Record;
