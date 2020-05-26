f21a8c7585cb49e2c43c34f29606aa84ad5e7c43
Adrien Mazet <adrien.mazet.56@gmail.com>
2020-05-26 15:36:07 +0200

    Signup page

--- packages/front/src/pages/signup/index.tsx
+++ packages/front/src/pages/signup/index.tsx
@@ -1,110 +1,27 @@
-import { QuestionCircleOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Tooltip } from "antd";
+import { UserAddOutlined } from "@ant-design/icons";
+import { Avatar, Typography } from "antd";
+import SignupForm from "containers/signup";
 import React from "react";
-import { Link } from "react-router-dom";
-import { register } from "api/authentication";
-import { Store } from "antd/lib/form/interface";
-import { Rule } from "antd/lib/form";
 
-const { Item } = Form;
+const { Title } = Typography;
 
-const Inscription = () => {
-  //const [form] = Form.useForm(); // why use a hook here and not in the other ?
-  const onFinish = (values: Store) => {
-    console.log("Received values of form: ", values);
-    const { username, email, password } = values;
-    register(username, email, password)
-      .then((res) => console.log(res.data))
-      .catch((error) => console.log(error));
-  };
-  return (
-    <Form
-      //form={form}
-      name="inscription"
-      onFinish={onFinish}
-      style={{ maxWidth: "500px", margin: "auto" }}
-    >
-      <Item
-        name="username"
-        label={
-          <span>
-            Username&nbsp;
-            <Tooltip title="The username used to connect to your account !">
-              <QuestionCircleOutlined />
-            </Tooltip>
-          </span>
-        }
-        rules={[
-          {
-            required: true,
-            message: "Please specify an username !",
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Item>
-      <Item
-        name="email"
-        label="Email adress"
-        rules={[
-          {
-            type: "email",
-            message: "Please input a valid email adress !",
-          },
-          {
-            required: true,
-            message: "Please specify an email adress !",
-          },
-        ]}
-      >
-        <Input />
-      </Item>
-      <Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Please specify a password !",
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Item>
-
-      <Item
-        name="confirm"
-        label="Password confirmation"
-        dependencies={["password"]}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please retype your password !",
-          },
-          ({ getFieldValue }: any) => ({
-            validator(rule, value) {
-              if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-              }
-
-              return Promise.reject("The password are not the same !");
-            },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Item>
-      <Item>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-        or <Link to="/login">login</Link>
-      </Item>
-    </Form>
-  );
-};
+const Signup = () => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Title>Signup</Title>
+    <Avatar
+      size={100}
+      style={{ marginBottom: 35 }}
+      icon={<UserAddOutlined />}
+    />
+    <SignupForm />
+  </div>
+);
 
 export default Signup;
