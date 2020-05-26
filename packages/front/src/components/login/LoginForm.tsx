f112400bae5e35a2ea0f3b09b3148a122741a386
Adrien Mazet <adrien.mazet.56@gmail.com>
2020-05-26 12:11:29 +0200

    Useless brackets removed

--- packages/front/src/components/login/LoginForm.tsx
+++ packages/front/src/components/login/LoginForm.tsx
@@ -11,67 +11,61 @@ type Props = {
 
 const { Item } = Form;
 
-const LoginForm = (props: Props) => {
-  return (
-    <Form
-      name="login"
-      style={{ width: "300px" }}
-      initialValues={{
-        email: localStorage.getItem("email"),
-        remember: true,
-      }}
-      onFinish={props.onFinish}
+const LoginForm = (props: Props) => (
+  <Form
+    name="login"
+    style={{ width: "300px" }}
+    initialValues={{
+      email: localStorage.getItem("email"),
+      remember: true,
+    }}
+    onFinish={props.onFinish}
+  >
+    <Item
+      name="email"
+      rules={[
+        {
+          type: "email",
+          message: "Please enter a valid email adress",
+        },
+        {
+          required: true,
+          message: "Please enter your email",
+        },
+      ]}
     >
-      <Item
-        name="email"
-        rules={[
-          {
-            type: "email",
-            message: "Please enter a valid email adress",
-          },
-          {
-            required: true,
-            message: "Please enter your email",
-          },
-        ]}
+      <Input prefix={<UserOutlined />} placeholder="Email" />
+    </Item>
+    <Item
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: "Please enter a password",
+        },
+      ]}
+    >
+      <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+    </Item>
+    <Item>
+      <Item name="remember" valuePropName="checked" noStyle>
+        <Checkbox>Remember me</Checkbox>
+      </Item>
+    </Item>
+    <Item>
+      <Button
+        type="primary"
+        htmlType="submit"
+        style={{
+          width: "100%",
+        }}
+        disabled={props.loading}
       >
-        <Input prefix={<UserOutlined />} placeholder="Email" />
-      </Item>
-      <Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please enter a password",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined />}
-          type="password"
-          placeholder="Password"
-        />
-      </Item>
-      <Item>
-        <Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Item>
-      </Item>
-      <Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          style={{
-            width: "100%",
-          }}
-          disabled={props.loading}
-        >
-          Login
-        </Button>
-        or <Link to="/signup">Sign up</Link>
-      </Item>
-    </Form>
-  );
-};
+        Login
+      </Button>
+      or <Link to="/signup">Sign up</Link>
+    </Item>
+  </Form>
+);
 
 export default LoginForm;
