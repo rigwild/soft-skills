7e75744d9aab0bbeee34db3b4e491de0bf75f31d
Adrien Mazet <adrien.mazet.56@gmail.com>
2020-05-29 19:04:52 +0200

    Modal added to profile form component

--- packages/front/src/components/profile/ProfileForm.tsx
+++ packages/front/src/components/profile/ProfileForm.tsx
@@ -1,9 +1,13 @@
-import { UserDeleteOutlined } from "@ant-design/icons";
-import { Button, Typography } from "antd";
+import {
+  ExclamationCircleOutlined,
+  UserDeleteOutlined,
+} from "@ant-design/icons";
+import { Button, Modal, Typography } from "antd";
 import React, { CSSProperties } from "react";
 
 const WIDTH = 360;
 const { Text, Paragraph } = Typography;
+const { confirm } = Modal;
 
 type Props = {
   name: string;
@@ -11,7 +15,7 @@ type Props = {
   joinDate: string;
   setName: (name: string) => void;
   editProfile: () => void;
-  showDeleteModal: () => void;
+  deleteProfile: () => void;
 };
 
 const labelContainerStyle: CSSProperties = {
@@ -20,44 +24,59 @@ const labelContainerStyle: CSSProperties = {
   width: WIDTH,
 };
 
-const ProfileForm = (props: Props) => (
-  <>
-    <div style={labelContainerStyle}>
-      <Text strong>Name :</Text>
-      <Paragraph
-        editable={{ onChange: props.setName }}
-        style={{ marginLeft: 15, width: "80%" }}
+const ProfileForm = (props: Props) => {
+  const showDeleteModal = () => {
+    confirm({
+      title: "Are you sure you want to delete your account ?",
+      icon: <ExclamationCircleOutlined />,
+      content: "This action can't be undone",
+      style: { top: 330 },
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk: props.deleteProfile,
+    });
+  };
+
+  return (
+    <>
+      <div style={labelContainerStyle}>
+        <Text strong>Name :</Text>
+        <Paragraph
+          editable={{ onChange: props.setName }}
+          style={{ marginLeft: 15, width: "80%" }}
+        >
+          {props.name}
+        </Paragraph>
+      </div>
+      <div style={labelContainerStyle}>
+        <Text strong>Email address :</Text>
+        <Paragraph style={{ marginLeft: 15 }}>{props.email}</Paragraph>
+      </div>
+      <div style={labelContainerStyle}>
+        <Text strong>Join date :</Text>
+        <Paragraph style={{ marginLeft: 15 }}>
+          {new Date(props.joinDate).toLocaleDateString()}
+        </Paragraph>
+      </div>
+      <Button
+        type="primary"
+        onClick={props.editProfile}
+        style={{ width: WIDTH, marginTop: 15 }}
       >
-        {props.name}
-      </Paragraph>
-    </div>
-    <div style={labelContainerStyle}>
-      <Text strong>Email address :</Text>
-      <Paragraph style={{ marginLeft: 15 }}>{props.email}</Paragraph>
-    </div>
-    <div style={labelContainerStyle}>
-      <Text strong>Join date :</Text>
-      <Paragraph style={{ marginLeft: 15 }}>
-        {new Date(props.joinDate).toLocaleDateString()}
-      </Paragraph>
-    </div>
-    <Button
-      type="primary"
-      onClick={props.editProfile}
-      style={{ width: WIDTH, marginTop: 15 }}
-    >
-      Apply changes
-    </Button>
-    <Button
-      type="primary"
-      danger
-      onClick={props.showDeleteModal}
-      icon={<UserDeleteOutlined />}
-      style={{ width: WIDTH, marginTop: 15 }}
-    >
-      Delete profile
-    </Button>
-  </>
-);
+        Apply changes
+      </Button>
+      <Button
+        type="primary"
+        danger
+        onClick={showDeleteModal}
+        icon={<UserDeleteOutlined />}
+        style={{ width: WIDTH, marginTop: 15 }}
+      >
+        Delete profile
+      </Button>
+    </>
+  );
+};
 
 export default ProfileForm;
