b6bd9c067f0805ccf7ad1700a6a72cdca070ef14
Adrien Mazet <adrien.mazet.56@gmail.com>
2020-06-02 23:39:55 +0200

    old route removed

--- packages/back/src/router.ts
+++ packages/back/src/router.ts
@@ -89,8 +89,6 @@ router.post(
   asyncMiddleware(authController.register)
 )
 
-router.post('/upload', authenticatedMiddleware(), asyncMiddleware(analyzisController.upload))
-
 /**
  * @api {post} /uploads Upload a file for analyzis
  * @apiVersion 0.1.0
